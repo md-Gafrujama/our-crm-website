c31219779a0e0e8632b4087d7c975505a2decbe0
Md Gafrujama Ansari <mdgafrujamaansri0786@gmail.com>
2025-07-01 19:28:48 +0530

    commit changes and upadted vercel.json

--- server-crm/client/src/Pages/protectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-function ProtectedRoute() {
-  const isLoggedIn = window.localStorage.getItem("loggedIn");
-  return isLoggedIn === "true" 
-    ? React.createElement(Outlet) 
-    : React.createElement(Navigate, { to: "/login" });
-}
-export default ProtectedRoute;
