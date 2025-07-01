ca336c0aec3c6428bb6c1234de274995cea460a9
Md Gafrujama Ansari <mdgafrujamaansri0786@gmail.com>
2025-07-01 22:45:13 +0530

    commit changes

--- server-crm/index.js
+++ server-crm/index.js
@@ -25,14 +25,13 @@ import updatePassword from "./middleware/updatePassword.middleware.js";
 import jwtTokenMiddleware from "./middleware/jwtoken.middleware.js"; 
 
 const app = express();
-// app.use(cors({
-//   origin: 'http://localhost:5173', 
-//   credentials: true,
-//   exposedHeaders: ['Content-Disposition'] 
-// }));
-// app.use(express.json()); 
-app.use(cors())
-app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true,
+  exposedHeaders: ['Content-Disposition'] 
+}));
+app.use(express.json()); 
+
 const port =  3333;
 
 connectDB();
