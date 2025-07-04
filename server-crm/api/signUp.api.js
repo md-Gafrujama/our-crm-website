db909188dbf8d99119aef50efd8afecb0ac1a573
MD GAFRUJAMA ANSARI <156898012+md-Gafrujama@users.noreply.github.com>
2025-07-04 20:27:03 +0530

    commit changes

--- server-crm/api/signUp.api.js
+++ server-crm/api/signUp.api.js
@@ -10,7 +10,7 @@ router.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', 'https://our-crm-website-99fa.vercel.app , http://localhost:5173, https://our-crm-website.vercel.app'); 
   res.header('Access-Control-Allow-Origin', 'https://our-crm-website-99fa.vercel.app'); 
   res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Methods', 'GET POST PUT DELETE OPTIONS');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
