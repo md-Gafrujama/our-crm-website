568bfb997f42c310b59f7051950da3665bbc39f3
MD GAFRUJAMA ANSARI <156898012+md-Gafrujama@users.noreply.github.com>
2025-07-04 18:45:14 +0530

    resolve add user error

--- server-crm/api/signUp.api.js
+++ server-crm/api/signUp.api.js
@@ -6,6 +6,14 @@ import path from 'path';
 import jwtTokenMiddleware from "../middleware/jwtoken.middleware.js";
 
 const router = express.Router();
+router.use((req, res, next) => {
+  // res.header('Access-Control-Allow-Origin', 'https://our-crm-website-99fa.vercel.app , http://localhost:5173, https://our-crm-website.vercel.app'); 
+  res.header('Access-Control-Allow-Origin', 'https://our-crm-website-99fa.vercel.app'); 
+  res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 router.post("/", upload.single('profilePhoto'),  uploadToCloudinary, async (req, res) => {
   try {
